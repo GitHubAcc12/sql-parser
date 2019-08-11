64bdb21a6162966332bd3bc06502f54cd6840caf
Jan Kretschmann <jankretschmann1@gmail.com>
2019-08-11 18:36:31 -0500

    jetlag... added main class and links for later inspiration

---
+++ src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+import java.io.File;
+
+// https://stackoverflow.com/questions/10379956/parsing-sql-like-syntax-design-pattern
+// https://stackoverflow.com/questions/2842809/lexers-vs-parsers
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (String arg :
+                args) {
+             File input = new File(arg);
+
+        }
+    }
+}
